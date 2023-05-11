b93017ae153809dd28b8f043a7a38db5121b772e
Jay McMullen <jay.mcmullen@qantashotels.com>
2023-05-11 11:04:44 +1000

    feat: .env to json

---
+++ README.md
@@ -0,0 +1,20 @@
+# Dot Env to JSON
+
+A tiny cli tool to convert .env files to JSON.
+
+---
+
+### Requirements
+
+- [Node](https://nodejs.org/en/download)
+- [Yarn](https://yarnpkg.com/en/docs/install)
+
+### Installation
+
+```bash
+# install dependencies
+yarn install
+
+# Run the cli tool
+yarn start
+```
---
+++ index.mjs
@@ -0,0 +1,15 @@
+import dotenv from 'dotenv';
+import inquirer from 'inquirer';
+import clipboardy from 'clipboardy';
+
+inquirer
+  .prompt([{ type: 'editor', name: 'env', message: 'Paste in your .env file' }])
+  .then(({ env }) => {
+    const result = dotenv.parse(env);
+    const json = JSON.stringify(result, null, 2);
+    clipboardy.writeSync(json);
+    console.log('✅ Successfully copied JSON to clipboard.');
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
